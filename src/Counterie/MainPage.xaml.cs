382381228dca991e8f5c6b8308b65763afc28ebe
Manuel Espinosa <106558272+manuelesvi@users.noreply.github.com>
2022-12-26 17:34:39 -0600

    Add welcome message

--- src/Counterie/MainPage.xaml.cs
+++ src/Counterie/MainPage.xaml.cs
@@ -1,49 +1,99 @@
-﻿using System.Globalization;
+﻿using Humanizer;
+using System.Diagnostics;
+using System.Globalization;
 using System.Speech.Synthesis;
 using Timer = System.Timers.Timer;
 namespace MauiApp3;
+#pragma warning disable CA1416 // Validate platform compatibility
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
     bool isStarted;
     readonly SpeechSynthesizer synthesizer = new SpeechSynthesizer();
-    readonly VoiceInfo selectedVI;
-    readonly Timer countTimer = new Timer(3300);
+    VoiceInfo selectedVI;
+    readonly Timer countTimer = new Timer(4000);
+    static int steps = 0;
+    static readonly CultureInfo _frCI = CultureInfo.GetCultureInfo("fr-FR");
 
     public MainPage()
-	{
-		InitializeComponent();
-		synthesizer.SelectVoiceByHints(VoiceGender.NotSet, 
-			VoiceAge.NotSet, 0, CultureInfo.GetCultureInfo("fr-FR"));
+    {
+        InitializeComponent();
+
+        synthesizer.SelectVoiceByHints(VoiceGender.NotSet,
+            VoiceAge.NotSet, 0, _frCI);
         selectedVI = synthesizer.Voice;
 
+        var voices = synthesizer.GetInstalledVoices();
+        var items = voices
+            .Select(v => (v.VoiceInfo.Name, v.VoiceInfo.Culture.DisplayName))
+            .ToArray();
+
+        int index = 0, selectedIndex = -1;
+        foreach (var item in items)
+        {
+            string shortened = item.Name;
+            shortened = shortened.Replace("Microsoft", string.Empty);
+            shortened = shortened.Replace("Desktop", string.Empty);
+            VoicesPck.Items.Add(string.Format("{0} [{1}]", shortened, item.DisplayName));
+            index++;
+
+            if (item.Name == selectedVI.Name)
+            {
+                selectedIndex = index;
+            }
+        }
+
+        VoicesPck.SelectedIndex = index;
+
         synthesizer.SpeakProgress += (s, e) =>
+        {
             SpeakingLbl.Text = e.Text;
-
-        this.Loaded += (s, e) =>
-        synthesizer.SpeakAsync(@"Bonjour tout le monde, 
-parlant via la technologie de voix au texte de Microsoft.
-Appuyez sur les boutons pour écouter les chiffres en français");
-
-        countTimer.Elapsed += (s, e) => {
-            SayNumber(PromptBreak.Large);
-            
-            MoreBtn_Clicked(s, e);
         };
-	}
+
+        Loaded += (s, e) => Welcome();
+    }
+
+    private void Welcome()
+    {
+        string message = selectedVI.Culture.TwoLetterISOLanguageName switch {
+            "en" => @"Welcome, I am talking with Microsoft's text to speech technology.
+Press the lower buttons to hear numbers in english",
+            
+            "es" => @"Bienvenido, hablo con tecnología de texto a voz de Microsoft.
+Presiona los botones inferiores para oir los números en español",
+
+            "fr" or _ => @"
+Bonjour tout le monde, 
+parlant via la technologie de voix au texte de Microsoft.
+Appuyez sur les boutons pour écouter les chiffres en français",
+        };
+
+        synthesizer.SpeakAsync(message);
+    }
+
+    private void CountTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+    {
+        Interlocked.Increment(ref count);
+        var builder = SayNumber(PromptBreak.Medium, false, false);
+        SayNumber(builder, PromptBreak.None, true, true);
+
+#if DEBUG
+        Interlocked.Increment(ref steps);
+        Debug.WriteLine($"Step {steps}, counter = {count}");
+#endif
+    }
 
     private void SayText(string text)
     {
-#pragma warning disable CA1416 // Validate platform compatibility
         synthesizer.SpeakAsync(text);
-#pragma warning restore CA1416 // Validate platform compatibility
     }
 
     private void Reset()
     {
         if (int.TryParse(ResetEntry.Text,
-                        NumberStyles.Integer, null, out int newCount))
+                        NumberStyles.Integer,
+                        null, out int newCount))
         {
             count = newCount;
             SayNumber();
@@ -52,18 +102,20 @@ Appuyez sur les boutons pour écouter les chiffres en français");
 
     private void StartCounter()
     {
+        countTimer.Elapsed += CountTimer_Elapsed;
         countTimer.Start();
-        // TODO: Change UI to pause
-        PlayBtn.Text = "\u23F8";
-        PlayBtn.BackgroundColor = Colors.Yellow;
         isStarted = true;
+        PlayBtn.Text = "\u23F8"; // pause
+        PlayBtn.BackgroundColor = Colors.Yellow;
     }
 
     private void StopCounter()
     {
+        countTimer.Elapsed -= CountTimer_Elapsed;
         countTimer.Stop();
         isStarted = false;
-        PlayBtn.Text = "\u23F5";
+        Interlocked.Exchange(ref steps, 0);
+        PlayBtn.Text = "\u23F5"; // play
         PlayBtn.BackgroundColor = Colors.Green;
     }
 
@@ -80,28 +132,49 @@ Appuyez sur les boutons pour écouter les chiffres en français");
     private void OnCounterClicked(object sender, EventArgs e)
     {
         SayNumber();
-        SemanticScreenReader.Announce(CounterBtn.Text);
+        // from template: SemanticScreenReader.Announce(CounterBtn.Text);
     }
 
-    private void SayNumber(PromptBreak promptBreak = PromptBreak.Small)
+    private PromptBuilder SayNumber(
+        PromptBreak promptBreak = PromptBreak.Small,
+        bool sayIt = true, bool endVoice = true)
     {
-        if (count == 1)
-            CounterBtn.Text = $"{count}";
-        else
-            CounterBtn.Text = $"{count}"; ;
+        var builder = new PromptBuilder();
+        builder.StartVoice(selectedVI);
+        builder = SayNumber(builder, promptBreak, endVoice, sayIt);
 
+        return builder;
+    }
+
+    private PromptBuilder SayNumber(
+        PromptBuilder builder,
+        PromptBreak promptBreak = PromptBreak.Small,
+        bool endVoice = true,
+        bool sayIt = true)
+    {
         //string veces = count > 1 ? "fois" : "fois";
 
         //synthesizer.SpeakAsync(
         //    $"bouton cliqué {count} {veces}.");
+        int presentCount = count; // pass a copy
+        Dispatcher.Dispatch(() => 
+        {
+            NumberLbl.Text = count.ToWords(selectedVI.Culture); // humanized :D
+            CounterBtn.Text = $"{count}";
+        });
 
-        var builder = new PromptBuilder();
-        builder.StartVoice(selectedVI);
         builder.AppendTextWithHint(count.ToString(), SayAs.NumberOrdinal);
         builder.AppendBreak(promptBreak);
-        builder.EndVoice();
+        if (endVoice)
+        {
+            builder.EndVoice();
+        }
+        if (sayIt)
+        {
+            synthesizer.SpeakAsync(builder);
+        }
 
-        synthesizer.SpeakAsync(builder);
+        return builder;
     }
 
     private void LessBtn_Clicked(object sender, EventArgs e)
@@ -112,7 +185,7 @@ Appuyez sur les boutons pour écouter les chiffres en français");
 
     private void MoreBtn_Clicked(object sender, EventArgs e)
     {
-		++count;
+        ++count;
         SayNumber();
     }
 
@@ -132,9 +205,13 @@ Appuyez sur les boutons pour écouter les chiffres en français");
         {
             // pause (count remains same)
             StopCounter();
-        } 
+        }
         else
         {
+            //SayNumber();
+            count++;
+            var builder = SayNumber(PromptBreak.Medium, false, false);
+            SayNumber(builder, PromptBreak.None, true, true);
             StartCounter();
         }
     }
@@ -147,5 +224,25 @@ Appuyez sur les boutons pour écouter les chiffres en français");
         SayNumber();
         // TODO: Change UI to play
     }
+
+    private void VoicesPck_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var voices = synthesizer.GetInstalledVoices();
+        string selectedV = VoicesPck.GetItemsAsArray()[VoicesPck.SelectedIndex];
+        selectedV = selectedV.Substring(0, selectedV.IndexOf("[")).TrimEnd();
+        
+        selectedVI = voices
+            .Where(v => v.VoiceInfo.Name.Contains(selectedV))
+            .Select(v => v.VoiceInfo)
+            .First();
+
+        synthesizer.SelectVoice(selectedVI.Name);
+    }
+
+    private void SayWelcome_Clicked(object sender, EventArgs e)
+    {
+        Welcome();
+    }
 }
 
+#pragma warning restore CA1416
